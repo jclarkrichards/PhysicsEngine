bedbf28ec019ea4fb7d6e269836d80ade552092a
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 16:14:35 -0500

    Update forces.py

--- forces.py
+++ forces.py
@@ -1,7 +1,23 @@
-class Force(object):
+from vectors import Vector2D
 
+class Force(object):
+    def __init__(self):
+        pass
+    
+    def updateForce(self, entity, dt):
+        pass
+    
 
 class Gravity(Force):
+    def __init__(self):
+        Force.__init__(self)
+        self.gravity = Vector2D(0, 100)
+    
+    def updateForce(self, entity, dt):
+        if entity.invMass != 0:
+            entity.addForce(self.gravity*entity.mass)
 
 
 class Drag(Force):
+    def __init__(self):
+        Force.__init__(self)
