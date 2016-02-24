efab6c4b50dbc5f3ba3fdb98501c3f9fc9d87b91
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 17:35:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 import pygame
 from pygame.locals import *
 from particle import Particle
+from forces import Gravity
 
 SCREENSIZE = (600,400)
 pygame.init()
 screen 
 
 p = Particle(40, 100)
+gravity = Gravity()
 
 while True:
-    
+    gravity.updateForce(particle)
     particle.update(dt)
     particle.render(screen)
