30908129a16140b512620857c8282ae9c2ea2d8b
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 15:50:30 -0500

    Update entity.py

--- entity.py
+++ entity.py
@@ -6,10 +6,21 @@ class Entity(object):
         self.position = Vector2D()
         self.velocity = Vector2D()
         self.acceleration = Vector2D()
+        self.Fnet = Vector2D()
+        self.mass = 0
+        self.invMass = 0
         
     def update(self, dt):
         pass
     
+    def setMass(self, mass):
+        self.mass = mass
+        if mass > 0:
+            self.invMass = 1.0 / mass
+    
+    def addForce(self, force):
+        self.Fnet += force
+        
     def render(self, screen):
         vals = list(self.position.toTuple()) + [2,2]
         pygame.draw.rect(screen, (200,0,0), vals)
