b6f07a7e4a3216c4e4049a75464f79eb9589f714
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 15:46:19 -0500

    Update particle.py

--- particle.py
+++ particle.py
@@ -1,3 +1,14 @@
 from entity import Entity
 
 class Particle(Entity):
+    def __init__(self):
+        Entity.__init__(self)
+    
+    def update(self, dt):
+        self.position += self.velocity*dt
+        self.acceleration = self.Fnet*self.invMass
+        self.velocity += self.acceleration*dt
+        self.Fnet = 0
+    
+    
+  
