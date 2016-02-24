a9a2d0cfce3878c453238b17400d46aaae844419
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 17:30:44 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 import pygame
 from pygame.locals import *
+from particle import Particle
+
+SCREENSIZE = (600,400)
+pygame.init()
+screen 
+
+p = Particle(40, 100)
 
 while True:
+    
+    particle.update(dt)
+    particle.render(screen)
