fd5e7e174914a64007fffe2d4be47f3d8e3d5c07
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 15:42:51 -0500

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,1 +1,16 @@
+from vectors import Vector2D
+
 class Entity(object):
+    def __init__(self):
+        self.ID = 0
+        self.position = Vector2D()
+        self.velocity = Vector2D()
+        self.acceleration = Vector2D()
+        
+    def update(self, dt):
+        pass
+    
+    def render(self, screen):
+        vals = list(self.position.toTuple()) + [2,2]
+        pygame.draw.rect(screen, (200,0,0), vals)
+    
