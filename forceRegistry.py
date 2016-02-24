20c0f5388beea32ca806b05b607e19c5b4651039
jclarkrichards <jclarkrichards@gmail.com>
2016-02-24 17:26:11 -0500

    Update forceRegistry.py

--- forceRegistry.py
+++ forceRegistry.py
@@ -1,3 +1,19 @@
 from forces import *
 
 class ForceRegistry(object):
+    def __init__(self):
+        self.registry = {}
+    
+    def add(self, entity, force):
+        pass
+    
+    def remove(self, entity, force):
+        pass
+    
+    def clear(self):
+        self.registry = {}
+    
+    def updateForces(self, dt):
+        pass
+    
+    
