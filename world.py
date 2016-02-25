6c9e6433768580774f690d09f61456417105b8e6
jclarkrichards <jclarkrichards@gmail.com>
2016-02-25 17:06:41 -0500

    Update world.py

--- world.py
+++ world.py
@@ -1,37 +1,44 @@
 class World(object):
-  def __init__(self):
-    self.staticOBJ = {}
-    self.dynamicOBJ = {}
+    def __init__(self):
+        self.staticOBJ = {}
+        self.dynamicOBJ = {}
     
-  def __addObject__(self, object):
-    pass
-    
-  def addStaticObject(self, object):
-    pass
+    def __addObject__(self, database, object):
+        if object.ID in database.keys():
+             return
+        if len(database) == 0:
+            database[0] = object
+            return
+        newID = max(database.keys()) + 1
+        object.ID = newID
+        database[newID] = object
+     
+    def addStaticObject(self, object):
+        self.__addObject__(self.staticOBJ, object)
   
-  def addDynamicObject(self, object):
-    pass
+    def addDynamicObject(self, object):
+        self.__addObject__(self.dynamicOBJ, object)
   
-  def removeStaticObject(self, object):
-    pass
+    def removeStaticObject(self, object):
+        pass
   
-  def removeDynamicObject(self, object):
-    pass
+    def removeDynamicObject(self, object):
+        pass
   
-  def update(self, dt):
-    pass
+    def update(self, dt):
+        pass
   
-  def integrateObjects(self, dt):
-    pass
+    def integrateObjects(self, dt):
+        pass
   
-  def clearStaticObjects(self):
-    pass
+    def clearStaticObjects(self):
+        pass
   
-  def clearDynamicObjects(self):
-    pass
+    def clearDynamicObjects(self):
+        pass
   
-  def clear(self):
-    pass
+    def clear(self):
+        pass
   
-  def render(self, screen):
-    pass
+    def render(self, screen):
+        pass
