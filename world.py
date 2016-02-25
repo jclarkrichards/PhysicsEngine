4feda8191dcf27a2986c36f058652e43ef8e93f5
jclarkrichards <jclarkrichards@gmail.com>
2016-02-25 16:02:56 -0500

    Update world.py

--- world.py
+++ world.py
@@ -1,1 +1,37 @@
 class World(object):
+  def __init__(self):
+    self.staticOBJ = {}
+    self.dynamicOBJ = {}
+    
+  def __addObject__(self, object):
+    pass
+    
+  def addStaticObject(self, object):
+    pass
+  
+  def addDynamicObject(self, object):
+    pass
+  
+  def removeStaticObject(self, object):
+    pass
+  
+  def removeDynamicObject(self, object):
+    pass
+  
+  def update(self, dt):
+    pass
+  
+  def integrateObjects(self, dt):
+    pass
+  
+  def clearStaticObjects(self):
+    pass
+  
+  def clearDynamicObjects(self):
+    pass
+  
+  def clear(self):
+    pass
+  
+  def render(self, screen):
+    pass
