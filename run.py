934b86756413d02e251e7d87865772160560cf1d
jclarkrichards <jclarkrichards@gmail.com>
2016-02-26 15:33:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from particle import Particle
 #from forces import Gravity
 from forceRegistry import ForceRegistry
 from world import World
+from entity import Entity
 
 SCREENSIZE = (600,400)
 pygame.init()
@@ -15,14 +16,19 @@ clock = pygame.time.Clock()
 particle = Particle(40, 100)
 particle.setMass(20)
 particle.setVelocity(40,-100)
-#particle.setID(0)
+
 particle2 = Particle(440, 100)
 particle2.setMass(40)
 particle2.setVelocity(-40,-100)
-#particle2.setID(1)
+
+#Set a static floor object
+floor = Entity(0,380)
+floor.setSize(600, 30)
+
 world = World()
 world.addDynamicObject(particle)
 world.addDynamicObject(particle2)
+world.addStaticObject(floor)
 
 #gravity = Gravity()
 registry = ForceRegistry()
