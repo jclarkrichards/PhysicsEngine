898cb58b0efa9248b369a81460e00d7129b4de18
jclarkrichards <jclarkrichards@gmail.com>
2016-02-26 15:11:19 -0500

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -3,10 +3,10 @@ from vectors import Vector2D
 class EntityContacts(object):
     def __init__(self):
         self.entities = [None, None]
-        self.restitution = 0
-        self.contactNormal = Vector2D()
-        self.penetration = 0
-        self.penetrateX, self.penetrateY = (0,0)
+        #self.restitution = 0
+        #self.contactNormal = Vector2D()
+        #self.penetration = 0
+        #self.penetrateX, self.penetrateY = (0,0)
     
     def setEntityPair(self, entity1, entity2):
         self.entities[0] = entity1
@@ -15,7 +15,7 @@ class EntityContacts(object):
     def resolve(self, dt):
         colliding = self.calculateCollision()
         if colliding:
-            self.resolvePenetration(dt)
+            self.resolveCollision(dt)
     
     def calculateCollision(self):
         '''Determine which collision detection to use here.  Assume rect to rect for now.'''
@@ -26,34 +26,35 @@ class EntityContacts(object):
         '''separting axis theorem for rectangles.  Need to include circles as well in the future'''
         xOverlap, yOverlap = (False, False)
         xValues = (self.entities[0].position.x, self.entities[1].position.x, self.entities[0].w, self.entities[1].w]
-        xOverlap, self.penetrateX = self.axisOverlap(*xValues)
+        xOverlap = self.axisOverlap(*xValues)
         if xOverlap:
             yValues = (self.entities[0].position.y, self.entities[1].position.y, self.entities[0].h, self.entities[1].h]
-            yOverlap, self.penetrateY = self.axisOverlap(*yValues)
+            yOverlap = self.axisOverlap(*yValues)
         return xOverlap & yOverlap
         
     def axisOverlap(self, p1, p2, l1, l2):
         '''check for axis overlap giving positions and lengths'''
         if p1 < p2:
             if p2+l2-p1 < l1+l2:
-                return True, p1+l1-p2
+                return True
         elif p1 > p2:
             if p1+l1-p2 < l1+l2:
-                return True, p2+l2-p1
+                return True
         elif p1 == p2:
             if l1 < l2:
-                return True, l1
+                return True
             elif l1 > l2:
-                return True, l2
-            return True, l2
-        return False, 0
+                return True
+            return True
+        return False
     
     def resolveVelocity(self, dt):
         pass
     
-    def resolvePenetration(self, dt):
+    def resolveCollision(self, dt):
         '''Separate objects so no longer penetrating.  Depends on objects shape.  Assume rectangles for now.  
         Also assumes entities[1] is static.'''
+        #Right before collision there was at least one separating axis
         previousPosition = self.entities[0].position - self.entities[0].velocity*dt
         xValues = (previousPosition.x, self.entities[1].position.x, self.entities[0].w, self.entities[1].w]
         xOverlap, dx = self.axisOverlap(*xValues)
