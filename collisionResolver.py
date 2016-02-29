627fb434b84b3c1f59e39f3df0e3797750814ed7
jclarkrichards <jclarkrichards@gmail.com>
2016-02-29 14:32:14 -0500

    Update collisionResolver.py

--- collisionResolver.py
+++ collisionResolver.py
@@ -1,17 +1,17 @@
+"""An entity pair is defined as entity1 and entity2.  """
 from vectors import Vector2D
 
 class EntityCollisionResolver(object):
     def __init__(self):
-        self.entities = [None, None]
+        self.entity1, self.entity2 = (None, None)
         #self.restitution = 0
         #self.contactNormal = Vector2D()
         #self.penetration = 0
         #self.penetrateX, self.penetrateY = (0,0)
     
     def setEntityPair(self, entity1, entity2):
-        self.entities[0] = entity1
-        self.entities[1] = entity2
-        
+        self.entity1, self.entity2 = (entity1, entity2)
+     
     def resolve(self, dt):
         colliding = self.calculateCollision()
         if colliding:
@@ -24,29 +24,22 @@ class EntityCollisionResolver(object):
     
     def calculateSeparatingAxes(self):
         '''separting axis theorem for rectangles.  Need to include circles as well in the future'''
-        xOverlap, yOverlap = (False, False)
-        xValues = (self.entities[0].position.x, self.entities[1].position.x, self.entities[0].w, self.entities[1].w)
-        xOverlap = self.axisOverlap(*xValues)
-        if xOverlap:
-            yValues = (self.entities[0].position.y, self.entities[1].position.y, self.entities[0].h, self.entities[1].h)
-            yOverlap = self.axisOverlap(*yValues)
-        return xOverlap & yOverlap
+        if xAxisGap(self.entity1.position, self.entity2.position):
+            return False
+        if yAxisGap(self.entity1.position, self.entity2.position):
+            return False
+        return True
+#------------------------------------------------------------------------------        
+    def xAxisGap(self, pos1, pos2):
+        '''Returns True if there is a gap'''
+        check1 = (pos1.x + self.entity1.w) < pos2.x
+        check2 = pos1.x > (pos2.x + self.entity.w)
+        return check1 or check2
         
-    def axisOverlap(self, p1, p2, l1, l2):
-        '''check for axis overlap giving positions and lengths'''
-        if p1 < p2:
-            if p2+l2-p1 < l1+l2:
-                return True
-        elif p1 > p2:
-            if p1+l1-p2 < l1+l2:
-                return True
-        elif p1 == p2:
-            if l1 < l2:
-                return True
-            elif l1 > l2:
-                return True
-            return True
-        return False
+    def yAxisGap(self, pos1, pos2):
+        check1 = (pos1.y + self.entity1.h) < pos2.y
+        check2 = pos1.y > (pos2.y + self.entity2.h)
+        return check1 or check2
     
     def resolveVelocity(self, dt):
         pass
@@ -54,32 +47,33 @@ class EntityCollisionResolver(object):
     def resolveCollision(self, dt):
         '''Separate objects so no longer penetrating.  
         Depends on objects shape.  Assume rectangles for now.  
-        Also assumes entities[1] is static.'''
+        Also assumes entity2 is static.'''
         #Right before collision there was at least one separating axis
-        previousPosition = self.entities[0].position - \
-                           self.entities[0].velocity*dt
-        xValues = (previousPosition.x, self.entities[1].position.x,
-                   self.entities[0].w, self.entities[1].w)
-        #print previousPosition, xValues
-        xOverlap = self.axisOverlap(*xValues)
-        #if xoverlap, assume y is the separating axis
-        if xOverlap:
-            if self.entities[0].position.y < self.entities[1].position.y:
-                self.entities[0].position.y = self.entities[1].position.y - \
-                                              self.entities[0].h
-                #self.entities[0].velocity = Vector2D(self.entities[0].velocity.x, 0)
-            elif self.entities[0].position.y > self.entities[1].position.y:
-                self.entities[0].position.y = self.entities[1].position.y + \
-                                              self.entities[1].h
-            self.entities[0].velocity = Vector2D(self.entities[0].velocity.x, 0)
+        #Get the previous position of entity1
+        pp = self.entity1.position - self.entity1.velocity*dt
+        
+        #if y is the separating axis
+        if yAxisGap(pp, self.entity2.position):
+            #Entity1 above entity2
+            if self.entity1.position.y < self.entity2.position.y:
+                self.entity1.position.y = self.entity2.position.y - \
+                                              self.entity1.h
+                                            
+            #Entity1 below entity2  
+            elif self.entity1.position.y > self.entity2.position.y:
+                self.entity1.position.y = self.entity2.position.y + \
+                                              self.entity2.h
+            self.entity1.velocity.y = 0.0
+            
         else: #assume x was the separating axis
-            if self.entities[0].position.x < self.entities[1].position.x:
-                self.entities[0].position.x = self.entities[1].position.x - \
-                                              self.entities[0].w
-            elif self.entities[0].position.x > self.entities[1].position.x:
-                self.entities[0].position.x = self.entities[1].position.x + \
-                                              self.entities[1].w
-            self.entities[0].velocity = Vector2D(0, self.entities[0].velocity.y)
+            if self.entity1.position.x < self.entity2.position.x:
+                self.entity1.position.x = self.entity2.position.x - \
+                                              self.entity1.w
+            elif self.entity1.position.x > self.entity2.position.x:
+                self.entity1.position.x = self.entity2.position.x + \
+                                              self.entity2.w
+            self.entity1.velocity.x *= -1
+            
     
 
     
