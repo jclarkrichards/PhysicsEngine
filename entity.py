cbf3948d93030d05ad02434b72d92ac962995f82
jclarkrichards <jclarkrichards@gmail.com>
2016-03-01 09:25:27 -0500

    Update entity.py

--- entity.py
+++ entity.py
@@ -4,10 +4,11 @@ from vectors import Vector2D
 class Entity(object):
     def __init__(self, x, y):
         self.ID = None
+        #Position vectors that describe the shape of the box
         self.min = Vector2D(x, y)
         self.max = Vector2D()
         
-        self.position = Vector2D(x, y)
+        #self.position = Vector2D(x, y)
         self.velocity = Vector2D()
         self.acceleration = Vector2D()
         self.Fnet = Vector2D()
@@ -16,16 +17,18 @@ class Entity(object):
         self.w, self.h = (0, 0)
         
     def update(self, dt):
-        self.position += self.velocity*dt
+        #self.position += self.velocity*dt
+        self.min += self.velocity.dt
+        self.max = self.min + self.size
         self.acceleration = self.Fnet #*self.invMass
         self.velocity += self.acceleration*dt
         self.Fnet = Vector2D()
     
     def setSize(self, width, height):
-        self.max = Vector2D(width, height)
-        
-        self.w = width
-        self.h = height
+        self.size = Vector2D(width, height)
+        self.max = self.min + self.size
+        #self.w = width
+        #self.h = height
         
     def setID(self, identification):
         self.ID = identification
@@ -42,6 +45,6 @@ class Entity(object):
         self.Fnet += force
         
     def render(self, screen):
-        vals = list(self.position.toTuple()) + [self.w, self.h]
+        vals = list(self.position.toTuple()) + list(self.size.toTuple())
         pygame.draw.rect(screen, (200,0,0), vals)
     
